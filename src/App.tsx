2330b599a4a00d0232188fee42ec34ccb492968e
yuanda <34027873+yuandahanif@users.noreply.github.com>
2022-12-22 01:05:42 +0700

    update poin

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import {
   FIRESTORE_ENTITY,
   useCollection,
+  useDocument,
   useFQuery,
 } from "./utils/useFirestore";
 
-import { f_user_upload } from "../types/firestore";
-import { doc, getFirestore, setDoc } from "firebase/firestore";
+import { f_pengguna, f_user_upload } from "../types/firestore";
+import {
+  DocumentReference,
+  DocumentSnapshot,
+  doc,
+  getDoc,
+  getFirestore,
+  setDoc,
+} from "firebase/firestore";
 import app from "./configs/firebase";
 
 export const db = getFirestore(app);
@@ -17,13 +25,37 @@ function App() {
     { fieldPath: "waktu_unggah", directionStr: "desc" }
   );
 
-  const onAction = (id: string, accept: boolean, rest: object) => {
+  const updateUserPoint = async (userId: string) => {
+    try {
+      const docRef = doc(
+        db,
+        FIRESTORE_ENTITY.pengguna.key,
+        userId
+      ) as DocumentReference<f_pengguna>;
+
+      const pengguna = await getDoc(docRef);
+      const point = pengguna.data()?.total_poin;
+      const new_poin = point != undefined ? point + 10 : 10;
+
+      setDoc(doc(db, FIRESTORE_ENTITY["pengguna"].key, pengguna.id), {
+        ...pengguna.data(),
+        total_poin: new_poin,
+      });
+
+      alert("berhasil");
+    } catch (error) {
+      console.log("file: App.tsx:47 ~ updateUserPoint ~ error", error);
+      alert("gagal");
+    }
+  };
+
+  const onAction = (id: string, accept: boolean, rest: f_user_upload) => {
     setDoc(doc(db, FIRESTORE_ENTITY["user-upload"].key, id), {
       ...rest,
       is_accepted: accept,
     })
-      .then(() => {
-        alert("berhasil");
+      .then(async () => {
+        updateUserPoint(rest.user_id.id);
       })
       .catch((error) => {
         alert("gagal");
